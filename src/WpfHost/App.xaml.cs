3bba321673a53871f08a5c5bb9bd1ae7a17b46fb
Trevor Balcom <tbalcom@herculesfreight.com>
2021-08-02 13:06:15 -0400

    Integrate Microsoft.Extensions.Configuration, DependencyInjection.

--- src/WpfHost/App.xaml.cs
+++ src/WpfHost/App.xaml.cs
@@ -1,17 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace WpfHost
+﻿namespace WpfHost
 {
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; private set; }
+        public IConfiguration Configuration { get; private set; }
+
+        public App()
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName))
+                .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        /// <summary>
+        /// Configure Dependency Injection...
+        /// </summary>
+        /// <param name="serviceCollection">Collection of services.</param>
+        private void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient(typeof(MainWindow));
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            ServiceProvider.GetRequiredService<MainWindow>()
+                .Show();
+        }
     }
 }
